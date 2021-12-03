6bac15033ba8a8fbf6722d823c96918eaca1bb6e
Matthew Latham <50916823+latham7@users.noreply.github.com>
2021-12-03 10:57:17 -0500

    Ping every 1 second

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 import os
 from ping3 import ping, verbose_ping
 import ping3
+import sched, time
+
 
 #ping3.DEBUG = True
 
-hostname='cammit.co'
+hostname='Google.com'
 
 def pingpong():
-    delay = ping3.ping(hostname)
-    delayMs = delay * 1000
-    print(hostname, delayMs, 'ms')
+    while True:
+
+        delay = ping3.ping(hostname)
+        delayMsLong = delay * 1000
+        delayMs = round(delayMsLong, 2)
+        print(hostname, delayMs, 'ms')
+        starttime = time.time()
+        time.sleep(1.0 - ((time.time() - starttime) % 60.0))
 
 pingpong()
 
